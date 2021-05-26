77ac771530393aae35d7ec2c719d9271ccc64ea7
Pierre Laffaille <pierre.laffaille@outlook.fr>
2021-05-26 13:49:34 +0200

    add playRound function
    call computerPlay and log playRound result

--- app.js
+++ app.js
@@ -10,3 +10,41 @@ function computerPlay() {
     return "scissors";
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  if (playerSelection === "rock" && computerSelection === "paper") {
+    console.log("p: " + playerSelection + " c: " + computerSelection);
+    computerScore++;
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    console.log("p: " + playerSelection + " c: " + computerSelection);
+    playerScore++;
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    console.log("p: " + playerSelection + " c: " + computerSelection);
+    playerScore++;
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    console.log("p: " + playerSelection + " c: " + computerSelection);
+    computerScore++;
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    console.log("p: " + playerSelection + " c: " + computerSelection);
+    computerScore++;
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    console.log("p: " + playerSelection + " c: " + computerSelection);
+    playerScore++;
+    `You win! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection == computerSelection) {
+    console.log("p: " + playerSelection + " c: " + computerSelection);
+    return "It's a tie!";
+  }
+}
+
+const playerSelection = prompt("Make your choice!", "").toLowerCase();
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
