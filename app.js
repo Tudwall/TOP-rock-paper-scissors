c45ab3589a48478892abf620e584415e2c921f8c
Pierre Laffaille <pierre.laffaille@outlook.fr>
2021-05-26 11:58:52 +0200

    refactor computerPlay function

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 // Generate a choice for the computer
 function computerPlay() {
   let num = Math.floor(Math.random() * 3);
-  let choice;
 
   if (num === 1) {
-    choice = "rock";
+    return "rock";
   } else if (num === 2) {
-    choice = "paper";
+    return "paper";
   } else {
-    choice = "scissors";
+    return "scissors";
   }
-  return choice;
 }
