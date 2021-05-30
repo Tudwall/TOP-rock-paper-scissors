9baa75e30df96263fe8076262b466267facb73e3
Pierre Laffaille <pierre.laffaille@outlook.fr>
2021-05-30 15:08:02 +0200

    select the html elements, add event listeners to the buttons
    display the score and round result in the divs

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+// Selectors.
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const score = document.querySelector("#score");
+const result = document.querySelector("#result");
+
+// Event Listeners.
+rockBtn.addEventListener("click", () => {
+  playerSelection = "rock";
+  computerSelection = computerPlay();
+  playRound(playerSelection, computerSelection);
+});
+paperBtn.addEventListener("click", () => {
+  playerSelection = "paper";
+  computerSelection = computerPlay();
+  playRound(playerSelection, computerSelection);
+});
+scissorsBtn.addEventListener("click", () => {
+  playerSelection = "scissors";
+  computerSelection = computerPlay();
+  playRound(playerSelection, computerSelection);
+});
+
+// Functions.
 // Generate a choice for the computer
 function computerPlay() {
   let num = Math.floor(Math.random() * 3);
@@ -13,43 +38,42 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return "It's a tie!";
+    score.textContent = "You: " + playerScore + " CPU: " + computerScore;
+    result.textContent = `It's a tie!`;
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
     playerScore++;
-    console.log("You: " + playerScore);
-    return `You win this round! ${playerSelection} beats ${computerSelection}`;
+    score.textContent = "You: " + playerScore + " CPU: " + computerScore;
+    result.textContent = `You win this round! ${playerSelection} beats ${computerSelection}`;
   } else if (
     (playerSelection === "rock" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "scissors") ||
     (playerSelection === "scissors" && computerSelection === "rock")
   ) {
     computerScore++;
-    console.log("CPU: " + computerScore);
-    return `You lose this round! ${computerSelection} beats ${playerSelection}`;
+    score.textContent = "You: " + playerScore + " CPU: " + computerScore;
+    result.textContent = `You lose this round! ${computerSelection} beats ${playerSelection}`;
   }
 }
 
 function game() {
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Make your choice!", "").toLowerCase();
-    let computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
-  }
+  // for (let i = 0; i < 5; i++) {
+  //   let playerSelection = prompt("Make your choice!", "").toLowerCase();
+  //   let computerSelection = computerPlay();
+  //   console.log(playRound(playerSelection, computerSelection));
+  // }
 
   if (playerScore === computerScore) {
-    console.log("It's a tie! " + playerScore + " to " + computerScore);
+    result.textContent = "It's a tie! " + playerScore + " to " + computerScore;
   } else if (playerScore < computerScore) {
-    console.log("CPU wins! " + computerScore + " to " + playerScore);
+    result.textContent = "CPU wins! " + computerScore + " to " + playerScore;
   } else if (playerScore > computerScore) {
-    console.log("You win! " + playerScore + " to " + computerScore);
+    result.textContent = "You win! " + playerScore + " to " + computerScore;
   }
 }
 
 let playerScore = 0;
 let computerScore = 0;
-
-game();
